d6a9fc4a4efa72ae18e49783c841532ee71e1cec
Luis Soto <lig2007@gmail.com>
2018-01-31 22:48:18 -0300

    prerado despachao

---
+++ CultimarWebApp/Utils/DAO/FactoryPreparadoDespacho.cs
@@ -0,0 +1,62 @@
+﻿using CultimarWebApp.Utils.Objetos;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CultimarWebApp.Utils.DAO
+{
+    public class FactoryPreparadoDespacho
+    {
+
+
+        //public List<ObjetoPreparadoDespacho> ListadoPreparadoDespechao()
+        //{
+        //    var ListadoSeguimientoLarval = new List<ObjetoPreparadoDespacho>();
+
+        //    var data = new DBConector().EjecutarProcedimientoAlmacenado("SP_GET_LISTADOSPreparadoDespacho", new System.Collections.Hashtable());
+
+        //    if (data.Rows.Count > 0)
+        //    {
+        //        for (var i = 0; i < data.Rows.Count; i++)
+        //        {
+        //            var validador = new object();
+        //            var resultadoListado = new ObjetoPreparadoDespacho();
+        //            validador = data.Rows[i].Field<object>("Id");
+        //            resultadoListado.Id = validador != null ? data.Rows[i].Field<int>("Id") : 1;
+
+        //            validador = data.Rows[i].Field<object>("CantidadDeLarvas");
+        //            resultadoListado.CantidadDeLarvas = validador != null ? data.Rows[i].Field<int>("CantidadDeLarvas") : 0;
+
+        //            validador = data.Rows[i].Field<object>("CosechaLarvas");
+        //            resultadoListado.CosechaLarvas = validador != null ? data.Rows[i].Field<int>("CosechaLarvas") : 0;
+
+        //            validador = data.Rows[i].Field<object>("NumeroEstanque");
+        //            resultadoListado.NumeroEstanque = validador != null ? data.Rows[i].Field<int>("NumeroEstanque") : 0;
+
+        //            validador = data.Rows[i].Field<object>("DensidadCultivo");
+        //            resultadoListado.DensidadCultivo = validador != null ? data.Rows[i].Field<int>("DensidadCultivo") : 0;
+
+        //            validador = data.Rows[i].Field<object>("FechaRegistro");
+        //            resultadoListado.FechaRegistro = validador != null ? data.Rows[i].Field<DateTime>("FechaRegistro") : DateTime.Today;
+
+        //            validador = data.Rows[i].Field<object>("FactoresMedicion");
+        //            resultadoListado.FactoresMedicion = validador != null ? data.Rows[i].Field<string>("FactoresMedicion") : "0";
+
+        //            validador = data.Rows[i].Field<object>("IdMortalidad");
+        //            resultadoListado.IdMortalidad = validador != null ? data.Rows[i].Field<int>("IdMortalidad") : 0;
+
+
+        //            ListadoSeguimientoLarval.Add(resultadoListado);
+        //        }
+        //    }
+        //    return ListadoSeguimientoLarval;
+        //}
+
+
+
+
+
+    }
+}
---
+++ CultimarWebApp/Utils/Objetos/ObjetoPreparadoDespacho.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CultimarWebApp.Utils.Objetos
+{
+    public class ObjetoPreparadoDespacho
+    {
+        private int _IdPreparoDespacho;
+        private string _FechaEnvio;
+        private string _FechaPreparado;
+        private int _IdOrigen;
+        private int _IdDestino;
+        private int _PesoNeto;
+        private int _PesoBruto;
+        private int _Cantidad;
+        private int _Calibre;
+        private string _Cliente;
+
+        public int IdTipoAlimento
+        {
+            get { return _IdPreparoDespacho; }
+            set { _IdPreparoDespacho = value; }
+        }
+        public string FechaEnvio
+        {
+            get { return _FechaEnvio; }
+            set { _FechaEnvio = value; }
+        }
+        public string FechaPreparado
+        {
+            get { return _FechaPreparado; }
+            set { _FechaPreparado = value; }
+        }
+        public int IdOrigen
+        {
+            get { return _IdOrigen; }
+            set { _IdOrigen = value; }
+        }
+        public int IdDestino
+        {
+            get { return _IdDestino; }
+            set { _IdDestino = value; }
+        }
+        public int PesoNeto
+        {
+            get { return _PesoNeto; }
+            set { _PesoNeto = value; }
+        }
+        public int PesoBruto
+        {
+            get { return _PesoBruto; }
+            set { _PesoBruto = value; }
+        }
+        public int Cantidad
+        {
+            get { return _Cantidad; }
+            set { _Cantidad = value; }
+        }
+        public int Calibre
+        {
+            get { return _Calibre; }
+            set { _Calibre = value; }
+        }
+        public string Cliente
+        {
+            get { return _Cliente; }
+            set { _Cliente = value; }
+        }
+
+    }
+}
